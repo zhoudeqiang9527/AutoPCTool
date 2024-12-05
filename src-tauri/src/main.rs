f1af074a032e064f49424ef841a073167f10e531
testuser01 <testuser01@nhkish.onmicrosoft.com>
2024-12-05 10:18:43 +0800

    初版做成

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod scan;
+use std::option;
+
+use scan::{scan_colors, scan_loop, scan_once, stop_scan};
+use tauri::{menu::{MenuBuilder, MenuItemBuilder}};
 fn main() {
-    mytool_lib::run()
+    tauri::Builder::default().setup(|app| {
+        let option = MenuItemBuilder::new("option").build(app)?;
+        let quite = MenuItemBuilder::new("quit").build(app)?;
+        let menu = MenuBuilder::new(app).items(&[&option, &quite]).build()?;
+        app.set_menu(menu)?;
+        app.on_menu_event(move |app, event| {
+            if event.id() == "option" {
+                app.exit(0);
+            } else if event.id() == "quit" {
+                app.exit(0);
+            }
+        });
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![scan_once, scan_loop, scan_colors, stop_scan])
+    .run(tauri::generate_context!())
+    .expect("error running tauri application");
+    
 }
---
+++ src-tauri/src/scan.rs
@@ -0,0 +1,73 @@
+use autopilot::{geometry::Point, mouse, screen};
+use lazy_static::lazy_static;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use std::thread;
+use std::time::Duration;
+
+lazy_static! {
+    static ref SHOULD_STOP: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+}
+
+#[tauri::command]
+pub fn scan_once(colors: Vec<[u8; 3]>, start_x: f64, end_x: f64, y: f64) -> bool {
+    // 调用 scan_colors 获取屏幕上的颜色
+    let screen_colors = scan_colors(start_x, end_x, y);
+    // 计算颜色差异
+    let mut is_match = true;
+    for i in 0..colors.len() {
+        if screen_colors[i] != colors[i] {
+            is_match = false;
+            break;
+        }
+    }
+    // 如果颜色匹配，则点击鼠标左键
+    if is_match {
+        //鼠标移动
+        let _ = mouse::move_to(Point::new(start_x + end_x / 2.0, y));
+        //鼠标左键点击
+        thread::sleep(Duration::from_millis(100));
+        let _ = mouse::click(mouse::Button::Left, None);
+    }
+    // 返回是否匹配
+    is_match
+}
+
+//间隔5秒 扫描
+#[tauri::command]
+pub fn scan_loop(colors: Vec<[u8; 3]>, start_x: f64, end_x: f64, y: f64, interval: u64) {
+    SHOULD_STOP.store(false, Ordering::Relaxed);
+    let stop_flg = SHOULD_STOP.clone();
+    thread::spawn(move || {
+        while !stop_flg.load(Ordering::Relaxed) {
+            scan_once(colors.clone(), start_x, end_x, y);
+            thread::sleep(Duration::from_millis(interval));
+        }
+    });
+}
+
+//停止扫描
+#[tauri::command]
+pub fn stop_scan() {
+    SHOULD_STOP.store(true, Ordering::Relaxed);
+}
+
+// 获取屏幕上某个点的颜色
+#[tauri::command]
+pub fn scan_colors(start_x: f64, end_x: f64, y: f64) -> Vec<[u8; 3]> {
+    let mut points: Vec<Point> = vec![];
+    let mut x = start_x;
+    while x < end_x {
+        points.push(Point::new(x, y));
+        x += 1.0;
+    }
+    // 循环获取坐标数组的颜色
+    let mut colors: Vec<[u8; 3]> = vec![];
+    for point in points {
+        let pixel = screen::get_color(point).unwrap();
+        colors.push([pixel[0], pixel[1], pixel[2]]);
+    }
+    colors
+}
